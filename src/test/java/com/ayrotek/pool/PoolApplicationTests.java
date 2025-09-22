627a283e5c6ef28707fd3dccc8035bb4073f9af4
enesozsahin <117269312+BoraEnesOzsahin@users.noreply.github.com>
2025-09-22 10:56:11 +0300

    Initial commit

---
+++ src/test/java/com/ayrotek/pool/PoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayrotek.pool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
